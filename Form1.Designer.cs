0402e7d7aaa824aea6aa9b175f62f6a6f5bee4d3
Zaki Omer <zakiomer624@gmail.com>
2024-12-09 17:19:28 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace LapFour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOperators = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMethod = new System.Windows.Forms.Label();
+            this.btnActMeth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(463, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Section 1 Lap Four";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(487, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "All Operators";
+            // 
+            // btnOperators
+            // 
+            this.btnOperators.Location = new System.Drawing.Point(398, 115);
+            this.btnOperators.Name = "btnOperators";
+            this.btnOperators.Size = new System.Drawing.Size(314, 83);
+            this.btnOperators.TabIndex = 1;
+            this.btnOperators.Text = "Check Operator";
+            this.btnOperators.UseVisualStyleBackColor = true;
+            this.btnOperators.Click += new System.EventHandler(this.btnOperators_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(487, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 27);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Methods";
+            // 
+            // lblMethod
+            // 
+            this.lblMethod.AutoSize = true;
+            this.lblMethod.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMethod.Location = new System.Drawing.Point(465, 308);
+            this.lblMethod.Name = "lblMethod";
+            this.lblMethod.Size = new System.Drawing.Size(145, 18);
+            this.lblMethod.TabIndex = 0;
+            this.lblMethod.Text = "Method Temp Code";
+            // 
+            // btnActMeth
+            // 
+            this.btnActMeth.Location = new System.Drawing.Point(398, 353);
+            this.btnActMeth.Name = "btnActMeth";
+            this.btnActMeth.Size = new System.Drawing.Size(314, 82);
+            this.btnActMeth.TabIndex = 2;
+            this.btnActMeth.Text = "Activate Method";
+            this.btnActMeth.UseVisualStyleBackColor = true;
+            this.btnActMeth.Click += new System.EventHandler(this.btnActMeth_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 658);
+            this.Controls.Add(this.btnActMeth);
+            this.Controls.Add(this.btnOperators);
+            this.Controls.Add(this.lblMethod);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOperators;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMethod;
+        private System.Windows.Forms.Button btnActMeth;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LapFour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Add Your Own Method
+            //ChangesLabelText();
+        }
+
+        //Methods
+        //access modifiers ( public [ access all projects ( Namespace ), private , protected + return type ( void [ doesnt return value ] , data type ) + MethodName + ( ... )
+        public void ChangesLabelText () //Parameters / agurment / var
+        {
+            lblMethod.Text = "The Text Changed";
+            lblMethod.ForeColor = Color.BlueViolet;
+        }
+
+        private void btnOperators_Click(object sender, EventArgs e)
+        {
+            //Operators
+
+            //1. Arthemtic Operators
+
+            //Create Var
+            int Num1, Num2;
+
+            //Instize the Vars
+            Num1 = 10;
+            Num2 = 20;
+
+            //Create another Var to store The Result
+            int Add, Subs, Multi, Div;
+
+            //Proccess ( ADD SUBS MULTI DIV )
+            Add = Num1 + Num2;
+            Subs = Num2 - Num1;
+            Multi = Num2 * Num1;
+            Div = Num1 / Num2;
+
+
+            //Showing the Result
+            MessageBox.Show("Addition of Num1 and Num 2 " + Add);
+            MessageBox.Show("Substraction of Num1 and Num2 " + Subs);
+            MessageBox.Show("Mutliplication of Num1 and Num2 " +  Multi);
+            MessageBox.Show("Division of Num1 and Num2 " + Div);
+
+
+            //2. Comparision
+
+            //Creating Var
+            bool Cond1, Cond2;
+            int Int1, Int2;
+
+            //Instizate Var
+            Cond1 = true;
+            Cond2 = false;
+            Int1 = 0;
+            Int2 = 1;
+
+            //Proccess ( Coditions [ Equal to , Not Equal to , Greater Than , Less Than ] )
+
+            //Create Outcome Condition
+            bool Result;
+
+            //1. Equal To: ( == )
+            Result = Cond1 == Cond2;
+            MessageBox.Show("Result Outcome " + Result);
+
+            //2. Not Equal to: ( ! ) ( = )
+            Result = Cond1 != Cond2;
+            MessageBox.Show("Result Outcome " + Result);
+
+            //3. Greater Than: ( > )
+            Result = Int1 > Int2;
+            MessageBox.Show("Result Outcome " + Result);
+
+            //4. Less Than: ( < )
+            Result = Int1 < Int2;
+            MessageBox.Show("Result Outcome " + Result);
+
+
+            //3. Logical Operators
+
+            //Create var
+            bool St1, St2;
+            bool ot;
+
+            //Instizale var
+            St1 = true;
+            St2 = false;
+
+
+            //Proccess (  Logical [ AND , OR , NOT ] )
+            //1. AND ( && )
+            ot = St1 && St2;
+            MessageBox.Show("Result Outcome " + ot);
+
+            //2. OR ( || )
+            ot = St1 || St2;
+            MessageBox.Show("Result Outcome " + ot);
+
+            //3. NOT ( != )
+            ot = St1 != St2;
+            MessageBox.Show("Result Outcome " + ot);
+
+
+            //4. Assgnement Operator ( = ) ( == )
+            // Var + ( = ) + value
+
+        }
+
+        private void btnActMeth_Click(object sender, EventArgs e)
+        {
+            //Active marka click event Start ( Bilamato )
+            ChangesLabelText();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Add Your Own Method
+            ChangesLabelText();
+        }
+    }
+}
